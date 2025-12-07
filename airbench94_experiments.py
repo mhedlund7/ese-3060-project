62282e9d31230aa493bbc800f3660f84182d17e3
mhedlund7 <mhedlund@seas.upenn.edu>
2025-12-07 01:30:29 -0500

    warmup

--- airbench94_experiments.py
+++ airbench94_experiments.py
@@ -506,6 +506,8 @@ if __name__ == "__main__":
     with open(sys.argv[0]) as f:
         code = f.read()
 
+    main('warmup')
+    
     print(f"Running Experiment Mode: {args.mode}")
     print_columns(logging_columns_list, is_head=True)
     
